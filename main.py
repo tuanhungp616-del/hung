5b2043b9f2c418c929273b49fcf79a2fa483b55e
tuanhungp616-del <tuanhungp616@gmail.com>
2026-03-19 23:56:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -208,4 +208,3 @@ async def home(): return FileResponse("index.html")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
-    
